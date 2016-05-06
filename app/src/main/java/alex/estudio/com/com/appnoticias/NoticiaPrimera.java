da32630bf11fe01d57388111519fc193dc16b537
aranda95 <alex.aranda1995@gmail.com>
2016-05-06 18:14:29 +0200

    Update NoticiaPrimera.java

--- app/src/main/java/alex/estudio/com/com/appnoticias/NoticiaPrimera.java
+++ app/src/main/java/alex/estudio/com/com/appnoticias/NoticiaPrimera.java
@@ -8,14 +8,11 @@ import android.widget.ImageView;
 import android.widget.TextView;
 
 public class NoticiaPrimera extends AppCompatActivity implements View.OnClickListener {
-
-
     private ImageButton atras;
     private ImageButton adelante;
     private ImageButton menu;
     private TextView textoAudi;
     private ImageView imagen;
-
     public int[] imagenesAudi = {R.drawable.audi1, R.drawable.audi2, R.drawable.audi3};
     private int posicion = 0, posicionFinal = imagenesAudi.length -1;
 
@@ -23,18 +20,14 @@ public class NoticiaPrimera extends AppCompatActivity implements View.OnClickLis
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_audi);
-
-
         atras = (ImageButton)findViewById(R.id.atrasAudi);
         adelante = (ImageButton)findViewById(R.id.adelanteAudi);
         menu = (ImageButton)findViewById(R.id.menuAudi);
         textoAudi = (TextView)findViewById(R.id.textoAudi);
         imagen = (ImageView)findViewById(R.id.imagen);
-
         atras.setOnClickListener(this);
         adelante.setOnClickListener(this);
         menu.setOnClickListener(this);
-
         textoAudi.setText("Audi A4 L, batalla extendida para China"+
                             "\n"+
                             "\nHoy comienza el Salón de Pekín 2016 y estamos viendo muchas novedades, entre ellas vehículos con batalla extendida, algo muy típico en China. Uno de los que se apuntan a este alargamiento de carrocería es el Audi A4 L. Este modelo que solo llegará al mercado chino crece hasta los 4,81 metros de largo, dejando la distancia entre ejes en 2,91 metros, lo que supone un incremento de 4 centímetros respecto a su predecesor."+
@@ -43,8 +36,6 @@ public class NoticiaPrimera extends AppCompatActivity implements View.OnClickLis
                             "\n"+
                             "\nSu gama de motores estará formada por dos mecánicas de gasolina de 2.0 litros TFSI. Estos cuatro cilindros vendrán con dos niveles de potencia: 190  o 252 CV. En ambos casos el cambio será automático de doble embrague y siete velocidades. De serie tendrá tracción delantera, pero opcionalmente el cliente podrá escoger la tracción total quattro. Las ventas del Audi A4 L comenzarán en China este verano.");
     }
-
-
         public void metodoatras(){
             if (posicion <= 0) {
                 posicion = posicionFinal;
@@ -53,7 +44,6 @@ public class NoticiaPrimera extends AppCompatActivity implements View.OnClickLis
             }
             imagen.setImageResource(imagenesAudi[posicion]);
         }
-
         public void metodoadelante(){
             if (posicion >= posicionFinal) {
                 posicion = 0;
@@ -62,17 +52,16 @@ public class NoticiaPrimera extends AppCompatActivity implements View.OnClickLis
             }
             imagen.setImageResource(imagenesAudi[posicion]);
         }
-
         @Override
-     public void onClick(View v) {
-    if (v.getId() == menu.getId()){
-     System.exit(0);
-     }
-     if (v.getId() == atras.getId()){
-      metodoatras();
-     }
-     if (v.getId() == adelante.getId()){
-      metodoadelante();
-     }
-}
+         public void onClick(View v) {
+            if (v.getId() == menu.getId()){
+             System.exit(0);
+             }
+             if (v.getId() == atras.getId()){
+            metodoatras();
+            }
+             if (v.getId() == adelante.getId()){
+             metodoadelante();
+        }
+    }
 }
